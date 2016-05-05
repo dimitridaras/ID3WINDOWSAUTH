9d65eec72abd129c5000f1464c09bc6242bbf33d
mark-truran <TruranM@onyxgroup.local>
2016-05-05 15:20:05 +0100

    Initial commit

---
+++ WebHost (Windows Auth)/Apis/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+using Microsoft.Owin.Cors;
+
+namespace Apis
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+
+            // accept access tokens from identityserver and require a scope of 'api1'
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "https://localhost:44333",
+
+                ClientId = "api",
+                ClientSecret = "api-secret",
+
+                RequiredScopes = new[] { "api" }
+            });
+
+            // configure web api
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            // require authentication for all controllers
+            config.Filters.Add(new AuthorizeAttribute());
+
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ WebHost (Windows Auth)/README.md
@@ -0,0 +1,6 @@
+Identity Server plus Windows Auth
+======================================
+
+This code demonstrates how to protect a web API using Identity Server 3 plus Windows Authentication. All of the code is taken from the Identity server 3 samples page. I have just put the bits together and written a walkthrough for beginners like me.
+
+I am happy to accept comments and pull requests.
